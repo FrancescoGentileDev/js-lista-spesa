1ad4d42f6454d0096820cbc9e22a85dd5ea6f20e
svalvastic <assogames2@gmail.com>
2022-08-30 16:34:48 +0200

    ✨ feat(script): add add to list

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,26 @@
 const htmlList = document.getElementById("list");
-let shoppingList = ["Latte", "Uova", "Prosciutto", "Salame", "Farina", "Mozzarella"]
-let count = 0; 
+let shoppingList = ["Latte", "Uova", "Prosciutto", "Salame", "Farina", "Mozzarella"];
+let count = 0;
 
+function addToList() {
 while (count < shoppingList.length) {
-    const element = document.createElement("li");
-    element.append(shoppingList[count])
-    htmlList.append(element)
-    count++;
+  const element = document.createElement("li");
+  element.innerHTML = `                    
+  <input type="checkbox" name="${shoppingList[count]}" id="${count}">
+    <label for="${shoppingList[count]}-${count}">${shoppingList[count]}</label>
+    `;
+  htmlList.append(element);
+  count++;
 }
+}
+addToList()
 
+
+const insert = document.getElementById('insert');
+
+insert.addEventListener('click', () => {
+    shoppingList.push(document.getElementById('addList').value) 
+    addToList()
+
+
+})
